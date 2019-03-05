c40195b002fd7f1ef05aea2cada4cb37441407ed
Martijn <m.reeuwijk@Live.nl>
2019-03-05 09:50:22 +0100

    first commit

---
+++ src/js/moduals/makestudieElement.js
@@ -0,0 +1,44 @@
+var fakedata = [
+  {
+    studie: "rechten",
+    icon: "rechten"
+  },
+  {
+    studie: "horica",
+    icon: "horica"
+  },
+  {
+    studie: "politiek",
+    icon: "politiek"
+  },
+  {
+    studie: "design",
+    icon: "design"
+  },
+  {
+    studie: "ict",
+    icon: "ict"
+  },
+  {
+    studie: "monteur",
+    icon: "monteur"
+  }
+];
+
+function makestudieElements() {
+  var elm = document.getElementsByClassName("iconBlockHolder")[0];
+  fakedata.forEach(data => {
+    elm.innerHTML += `
+    <div class="iconBlock borderRadius colorHover capitalize iconHover">
+      <div class="iconBlok-icon">
+      <a href="${data.name}">
+      <img src="src/image/icons/${data.icon}.svg" alt="${data.icon}" />
+      </a>
+      </div>
+      <p>${data.studie}</p>
+    </div>
+    `;
+  });
+  console.log(elm);
+}
+makestudieElements();
