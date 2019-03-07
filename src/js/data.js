ab41dce070d7ac7ee17d92b991d329b67459ea2e
Martijn <m.reeuwijk@Live.nl>
2019-03-07 15:49:03 +0100

    LEts goo

---
+++ src/js/data.js
@@ -0,0 +1,79 @@
+export const kasten = [
+  {
+    name: "economie",
+    etage: "5",
+    subjects: [
+      {
+        name: "deonderneming",
+        color: "green",
+        value: "351.1"
+      },
+      {
+        name: "economische_order",
+        color: "blue",
+        value: "351"
+      },
+      {
+        name: "belasting",
+        color: "yellow",
+        value: "346.2"
+      },
+      {
+        name: "openbare_financien",
+        color: "orange",
+        value: "346"
+      },
+      {
+        name: "waarde_en_prijsleer",
+        color: "purple",
+        value: "342"
+      }
+    ]
+  },
+  {
+    name: "rechten",
+    etage: "6",
+    subjects: [
+      {
+        name: "misdaden",
+        color: "orange",
+        value: "396"
+      },
+      {
+        name: "jeudgcriminaliteit",
+        color: "blue",
+        value: "396.7"
+      },
+      {
+        name: "familierecht",
+        color: "green",
+        value: "397.1"
+      },
+      {
+        name: "kinderrecht",
+        color: "purple",
+        value: "397.1"
+      },
+      {
+        name: "rechtspersoon",
+        color: "orange",
+        value: "397.2"
+      },
+      {
+        name: "erfrecht",
+        color: "orange",
+        value: "397.3"
+      },
+      {
+        name: "zakenrecht",
+        color: "orange",
+        value: "397.5"
+      },
+      {
+        name: "vernindingsrecht",
+        color: "black",
+        value: "397.6"
+      }
+    ]
+  }
+];
