f23805fab4d106a71506c880e230bf20211c285b
Martijn <m.reeuwijk@Live.nl>
2019-03-08 12:19:15 +0100

    urrgg removed link

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ import { removeMap } from "./module/map.js";
 import { makeIndex } from "./module/map.js";
 import { addClickMap } from "./module/map.js";
 import { removeClickMap } from "./module/map.js";
-import { getData } from "./module/api.js";
+// import { getData } from "./module/api.js";
 // import { API } from "/node_modules/oba-wrapper/js/index.js";
 // import { getStudie } from "./module/studie.js";
 
